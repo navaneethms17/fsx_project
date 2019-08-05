2e721a6c415c7bfe8fe3cf81d3521072bf24a68e
navaneethms17 <53570108+navaneethms17@users.noreply.github.com>
2019-08-05 19:48:03 +0530

    Update ComputerWorld.scala

--- src/test/scala/computerdatabase/ComputerWorld.scala
+++ src/test/scala/computerdatabase/ComputerWorld.scala
@@ -9,24 +9,53 @@ import io.gatling.http.Predef._
 
 class ComputerWorld extends Simulation {
 
-  val httpProtocol = http
-    .baseUrl("https://fsx-qa.pragiti.com")
-    .inferHtmlResources()
-    .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36")
+ val httpProtocol = http
+    .baseUrl("http://computer-database.gatling.io") // Here is the root for all relative URLs
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
+  val headers_10 = Map("Content-Type" -> "application/x-www-form-urlencoded") // Note the headers specific to a given request
 
-  val headers_10 = Map(
-    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    "Accept-Encoding" -> "gzip, deflate, br",
-    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
-    "Pragma" -> "no-cache",
-    "Upgrade-Insecure-Requests" -> "1")
+  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
+    .exec(http("request_1")
+      .get("/"))
+    .pause(7) // Note that Gatling has recorded real time pauses
+    .exec(http("request_2")
+      .get("/computers?f=macbook"))
+    .pause(2)
+    .exec(http("request_3")
+      .get("/computers/6"))
+    .pause(3)
+    .exec(http("request_4")
+      .get("/"))
+    .pause(2)
+    .exec(http("request_5")
+      .get("/computers?p=1"))
+    .pause(670 milliseconds)
+    .exec(http("request_6")
+      .get("/computers?p=2"))
+    .pause(629 milliseconds)
+    .exec(http("request_7")
+      .get("/computers?p=3"))
+    .pause(734 milliseconds)
+    .exec(http("request_8")
+      .get("/computers?p=4"))
+    .pause(5)
+    .exec(http("request_9")
+      .get("/computers/new"))
+    .pause(1)
+    .exec(http("request_10") // Here's an example of a POST request
+      .post("/computers")
+      .headers(headers_10)
+      .formParam("name", "Beautiful Computer") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
+      .formParam("introduced", "2012-05-30")
+      .formParam("discontinued", "")
+      .formParam("company", "37"))
 
+  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+}
 
-  val scn = scenario("homepage")
-    .exec(http("request_10")
-      .get("/fsxstorefront/fsx/en_US/")
-      .headers(headers_10))
-
-  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
